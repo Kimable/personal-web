511f393302080d3372a3943a490685ff71ce3507
Kimable <kkimani@yahoo.com>
2021-02-10 17:18:18 +0300

    more pages added

--- app.js
+++ app.js
@@ -11,5 +11,20 @@ app.get("/", (req, res) => {
   let items = ["Shoes", "Shirt", "Trousers"];
   res.render("index", { header: "KIM", items: items });
 });
+app.get("/about", (req, res) => {
+  let items = ["Shoes", "Shirt", "Trousers"];
+  res.render("about", { header: "KIM", items: items });
+});
+app.get("/projects", (req, res) => {
+  let items = ["Shoes", "Shirt", "Trousers"];
+  res.render("projects", { header: "KIM", items: items });
+});
+app.get("/contact", (req, res) => {
+  let items = ["Shoes", "Shirt", "Trousers"];
+  res.render("contact", { header: "KIM", items: items });
+});
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 
 app.listen(port, console.log(`We are live on port ${port}`));
