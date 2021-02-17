c9133255b93c278dd9f38cbffc9f322e5f2b0f19
Kimable <kkimani@yahoo.com>
2021-02-17 13:04:54 +0300

    edited some files

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+
+// Dot ENV
+require("dotenv").config();
+
 const app = express();
 const port = 5050;
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -23,6 +33,52 @@ app.get("/contact", (req, res) => {
   let items = ["Shoes", "Shirt", "Trousers"];
   res.render("contact", { header: "KIM", items: items });
 });
+
+// Contact middleware
+app.post("/send", (req, res) => {
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.zoho.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL, // generated ethereal user
+        pass: process.env.PASSWORD, // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Kelvin Contact Page" info@kkimani.com', // sender address
+      to: "info@kkimani.com, kelvn.kimani@gmail.com", // list of receivers
+      replyTo: req.body.email,
+      subject: "New Message from KK's Contact Form", // Subject line
+      html: `
+    <h1> New message from: ${req.body.name}</h1>
+    <p>Phone: ${req.body.phone}</p>
+    <p>Email: ${req.body.email}</p>
+    <p>Message: ${req.body.message}</p>
+    `, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+  res.redirect("/");
+
+  main().catch(console.error);
+});
+
+// 404 pages
 app.use((req, res) => {
   res.status(404).render("404");
 });
--- views/send.js
+++ views/send.js
@@ -3,24 +3,21 @@ const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
+  
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
+      user: "info@kkimani.com", // generated ethereal user
+      pass: "Mayfair@2005", // generated ethereal password
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Kelvin Contact Page" kelvin@kkimani.com', // sender address
+    from: '"Kelvin Contact Page" info@kkimani.com', // sender address
     to: "info@kkimani.com, kelvn.kimani@gmail.com", // list of receivers
     subject: "Hello ✔", // Subject line
     html: "<b>Hello world?</b>", // html body
